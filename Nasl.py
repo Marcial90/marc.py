a88b68c7278996bb74425b809b8735916907b859
Marcial90 <100608116+Marcial90@users.noreply.github.com>
2022-02-28 19:44:17 +0300

    Create Nasl.py

---
+++ Nasl.py
@@ -0,0 +1,24 @@
+import os
+os.system ("clear")
+
+print ('\033[0;33m     ___  ___       ___   _____    _____   _       ___   _      ')                                  print ('\033[0;33m    /   |/   |     /   | |  _  \  /  ___| | |     /   | | |     ')
+print ('\033[0;33m   / /|   /| |    / /| | | |_| |  | |     | |    / /| | | |     ')                                  print ('\033[0;33m  / / |__/ | |   / / | | |  _  /  | |     | |   / / | | | |     ')
+print ('\033[0;33m / /       | |  / /  | | | | \ \  | |___  | |  / /  | | | |___  ')                                  print ('\033[0;33m/_/        |_| /_/   |_| |_|  \_\ \____ | |_| /_/   |_| |_____| ')
+
+print ("\033[0;34m haider")
+
+
+
+
+#gmail = input ("\033[0;32m gmail :")
+
+
+#password = input ("\033[0;32m password :")
+
+import socket
+import sys
+import requests
+print ('\033[0;33m enter your DNS or Target : ')
+hostname = input ('\033[0;33m enter link :')
+ip=socket.gethostbyname(hostname)
+print ('\033[0;33m Host Name Is: ',hostname, '\n' '\033[0;31m Target Ip Is: ',ip)
